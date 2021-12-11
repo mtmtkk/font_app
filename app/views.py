3e8d25126229eb48f148b5cb1dcad1fe2472db21
mtmt <mtmtk5678@gmail.com>
2021-12-12 03:08:31 +0900

    commit

--- app/views.py
+++ app/views.py
@@ -15,6 +15,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import cv2
+import requests
 
 from django_cleanup import cleanup
 
@@ -35,26 +36,37 @@ def crop(request):
 def result(request):
     name = np.array(['MS ゴシック','MS 明朝','メイリオ','游ゴシック','UDデジタル教科書体'],dtype=object)
 
-    path = 'media/images/my-image.png'
-    is_file = os.path.isfile(path)
+    path = 'https://res.cloudinary.com/hriscwqpd/image/upload/v1638897819/media/images/my-image.png'
+    #is_file = os.path.isfile(path)
 
-    if is_file:
-        image = cv2.imread(path) 
-        image = cv2.resize(image,dsize=(224,224))
-        image = image/255.0
-        test_image = (np.expand_dims(image,0))
-        model = tf.keras.models.load_model(MODEL_ROOT)
-        predictions = model.predict(test_image)
-        #print(predictions)
-        predictions_sort = np.argsort(predictions[0])[::-1]
-        name_sort = name[predictions_sort]
-        result_sort = predictions[0][predictions_sort]
-    else:
-        return render(request,'app/index.html')
+    #if is_file:
+    image = imread_web(path)
+    image = cv2.resize(image,dsize=(224,224))
+    image = image/255.0
+    test_image = (np.expand_dims(image,0))
+    model = tf.keras.models.load_model(MODEL_ROOT)
+    predictions = model.predict(test_image)
+    predictions_sort = np.argsort(predictions[0])[::-1]
+    name_sort = name[predictions_sort]
+    result_sort = predictions[0][predictions_sort]
+   # else:
+       # print('not exit')
+        #return render(request,'app/index.html')
 
     context = {
         'name':name_sort,
         'result':result_sort
     }
-
     return render(request, 'app/result.html',context)
+
+def imread_web(url):
+    # 画像をリクエストする
+    res = requests.get(url)
+    img = None
+    # Tempfileを作成して即読み込む
+    fp = tempfile.NamedTemporaryFile(dir='./', delete=False)
+    fp.write(res.content)
+    fp.close()
+    img = cv2.imread(fp.name)
+    os.remove(fp.name)
+    return img
