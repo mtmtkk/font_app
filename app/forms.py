33b474d002c80fe056147e416722f934fe5c02c1
mtmt <mtmtk5678@gmail.com>
2021-12-17 22:51:02 +0900

    commit

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Image
+from cloudinary.forms import CloudinaryFileFiel
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['picture',]
+    image = CloudinaryFileField(
+            options={'folder': 'media/Model_image', 'tags': 'Model_name'})
