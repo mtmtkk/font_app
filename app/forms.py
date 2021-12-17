48ffce1bafe2b49d63faae2a812506c47777e151
mtmt <mtmtk5678@gmail.com>
2021-12-17 22:57:49 +0900

    test

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Image
-from cloudinary.forms import CloudinaryFileFiel
+from cloudinary.forms import CloudinaryFileField
 
 class ImageForm(forms.ModelForm):
     class Meta:
